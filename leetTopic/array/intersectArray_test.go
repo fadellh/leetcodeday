ff90d4091d5ee0fd1e3b261ec6461512aa053342
fadellh <fadel.lukman.dev@gmail.com>
2022-08-01 21:53:27 +0700

    leetcode 530

---
+++ leetTopic/array/intersectArray_test.go
@@ -0,0 +1,34 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_intersect(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				nums1: []int{4, 9, 5},
+				nums2: []int{9, 4, 9, 8, 4},
+			},
+			want: []int{4, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intersect(tt.args.nums1, tt.args.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("intersect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
