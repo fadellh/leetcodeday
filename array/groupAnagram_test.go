2ad29ecd74eeb59dd394b6a1c194eeb00bcf8fee
fadellh <fadel.lukman.dev@gmail.com>
2022-08-30 08:56:55 +0700

    leetcode 49

---
+++ array/groupAnagram_test.go
@@ -0,0 +1,32 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_groupAnagrams(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{
+			name: "1",
+			args: args{
+				strs: []string{"eat", "tea", "tan", "ate", "nat", "bat"},
+			},
+			want: [][]string{{"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := groupAnagrams(tt.args.strs); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
